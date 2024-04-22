13a49f41afe41b0ef6289e252ee917d7bc7684c5
Raysika <raymondsillakata@gmail.com>
2024-04-22 23:13:36 +0300

    added variable first name and its update function to track first name of user

--- lib/controllers/logincontroller.dart
+++ lib/controllers/logincontroller.dart
@@ -2,7 +2,11 @@ import 'package:get/get.dart';
 
 class LoginController extends GetxController {
   var email = ''.obs;
+  var firstName = ''.obs;
   updateEmail(string){
     email.value = string;
+  }
+  updateFirstName(string){
+    firstName.value = string;
   }
 }

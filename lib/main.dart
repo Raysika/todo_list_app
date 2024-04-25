f6ed03c36cfc78294e8f1eeb887a647b27a2d147
Raysika <raymondsillakata@gmail.com>
2024-04-25 13:12:16 +0300

    commented the temporarily ignore certificate errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
-import 'dart:io';
+// import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_app/utils/routes.dart';
 
-//Temporarily Ignore Certificate Errors (Development Only):
-class MyHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
-  }
-}
+// //Temporarily Ignore Certificate Errors (Development Only):
+// class MyHttpOverrides extends HttpOverrides {
+//   @override
+//   HttpClient createHttpClient(SecurityContext? context) {
+//     return super.createHttpClient(context)
+//       ..badCertificateCallback =
+//           (X509Certificate cert, String host, int port) => true;
+//   }
+// }
 void main() {
-  HttpOverrides.global = MyHttpOverrides();
+  //HttpOverrides.global = MyHttpOverrides();
   runApp(MyApp());
 }
 
